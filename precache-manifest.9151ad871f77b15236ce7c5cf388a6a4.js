98db21940dc9a51c09995a90f29df97237ae1be2
maxzcool <maxzcool@mail.ru>
2020-04-23 15:32:11 +0500

    Updates

---
+++ precache-manifest.9151ad871f77b15236ce7c5cf388a6a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b2792e7a0419664711737160a2d5a1",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "feaa82174a99c019ee9d",
+    "url": "/test/static/css/3.bb00c464.chunk.css"
+  },
+  {
+    "revision": "aa69af1c7659031c6ea9",
+    "url": "/test/static/css/main.cd909995.chunk.css"
+  },
+  {
+    "revision": "4352bf71a9b7389aeb60",
+    "url": "/test/static/js/2.f07d0db5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test/static/js/2.f07d0db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feaa82174a99c019ee9d",
+    "url": "/test/static/js/3.98b0c52b.chunk.js"
+  },
+  {
+    "revision": "42c028a867df68425441",
+    "url": "/test/static/js/4.972be1cc.chunk.js"
+  },
+  {
+    "revision": "aa69af1c7659031c6ea9",
+    "url": "/test/static/js/main.e9897989.chunk.js"
+  },
+  {
+    "revision": "418dee8a5339dda463fb",
+    "url": "/test/static/js/runtime-main.6e9486fa.js"
+  },
+  {
+    "revision": "36bfb496e70da6a15fad485ccee92662",
+    "url": "/test/static/media/15.36bfb496.svg"
+  },
+  {
+    "revision": "6de6290fe4610c3823730c983b3100af",
+    "url": "/test/static/media/bg1.6de6290f.jpg"
+  }
+]);
